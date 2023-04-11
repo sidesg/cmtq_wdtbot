3a31fc21ebc5c0bea51715a4f6c3d637d58d440e
sidesg <sidesg@gmail.com>
2023-04-11 15:03:56 -0400

    Refactor ajoutpersonnes, update base data

--- ajoutpersonnes/ajoutpersonnes.py
+++ ajoutpersonnes/ajoutpersonnes.py
@@ -2,45 +2,11 @@
 
 import pywikibot
 import polars as pl
+import pandas as pd
 
-def main(donnees, minoccs) -> None:
-    nouvelles_personnes = (
-        pl.scan_csv(donnees)
-        .filter(pl.col("occurrences") > minoccs)
-        .filter(pl.col("effectue").is_null())
-        .filter(pl.col("qid").is_null())
-        .collect()
-    )
-
-    #Traduire "foncts" en occupation qids
-    metiers_dict = creer_metiersdict()
-    foncts = nouvelles_personnes.select(pl.col("foncts").str.split(", ")).to_series()
-    foncts = foncts.apply(
-        lambda x : [metiers_dict.get(f, None) for f in x if metiers_dict.get(f, None)]
-    )
-    nouvelles_personnes.replace("foncts", foncts)
-
-    #Créer entités WDT
-    nouvelles_personnes = nouvelles_personnes.head(5)
-
-    site = pywikibot.Site("wikidata", "wikidata")
-    repo = site.data_repository()
-
-    for row in nouvelles_personnes.rows(named=True):
-        nomcomp = row["Prenom"] + " " + row["Nom"] if row["Prenom"] else row["Nom"]
-        
-        # assert type(row) == dict
-
-        new_item_id = create_item(repo, row, nomcomp)
-        
-        print(f"{nomcomp} : {new_item_id}")
-
-
-
-def resume_df(df: pl.DataFrame):
+def resume_df(df: pl.DataFrame) -> None:
     print(len(df))
     print(df.head())
-
 
 def creer_metiersdict(chemin: str="donnees/mapping_metiers.csv") -> dict:
     df = pl.read_csv(chemin).filter(pl.col('qid').is_not_null())
@@ -58,7 +24,7 @@ def ajout_source(decl: pywikibot.Claim, repo: pywikibot.DataSite) -> None:
 
     decl.addSource(ref, bot=True)
 
-def create_item(repo: pywikibot.DataSite, row: dict, nomcomp: str) -> str:
+def create_item(repo: pywikibot.DataSite, row: pd.Series, nomcomp: str) -> str:
     new_item = pywikibot.ItemPage(repo)
     new_item.editLabels(
         labels= {
@@ -68,9 +34,15 @@ def create_item(repo: pywikibot.DataSite, row: dict, nomcomp: str) -> str:
         summary="Setting labels"
     )
 
-    #TODO: Ajouter AKA
+    if pd.notna(row["voirnom"]):
+        aliases = {
+            "fr": row["voirnom"],
+            "en": row["voirnom"]
+        }
+        new_item.editAliases(aliases, summary="Setting new aliases.")
 
-    notes = row["Notes"] if row["Notes"] else "Personne liée au cinéma québécois"
+    notes = row["Notes"] if pd.notna(row["Notes"]) else "Personne liée au cinéma québécois"
+
     new_item.editDescriptions({"fr": notes}, summary="Setting new descriptions.")
 
     nature = pywikibot.Claim(repo, "P31")
@@ -94,8 +66,43 @@ def create_item(repo: pywikibot.DataSite, row: dict, nomcomp: str) -> str:
     return new_item.getID()
 
 
-if __name__ == "__main__":
-    donnees = "donnees/personnes_amapper.csv"
-    minoccs = 40
+def main(donnees: str, minoccs: int, qlimit: int) -> None:
+    datadf = pd.read_csv(donnees)
+    nouvelles_personnes = datadf[datadf["occurrences"] > minoccs]
+    nouvelles_personnes = nouvelles_personnes[nouvelles_personnes["effectue"].isnull()]
+    nouvelles_personnes = nouvelles_personnes[nouvelles_personnes["qid"].isnull()]
 
-    main(donnees, minoccs)
+    nouvelles_personnes["voirnom"] = nouvelles_personnes["voirnom"].str.split("|")
+    nouvelles_personnes["voirid"] = nouvelles_personnes["voirid"].str.split("|")
+
+    metiers_dict = creer_metiersdict()
+    nouvelles_personnes["foncts"] = nouvelles_personnes["foncts"].str.split(", ")
+
+    nouvelles_personnes["foncts"] = nouvelles_personnes["foncts"].apply(
+        lambda x : [metiers_dict.get(f, None) for f in x if metiers_dict.get(f, None)]
+    )
+
+    nouvelles_personnes = nouvelles_personnes.head(qlimit)
+
+    site = pywikibot.Site("wikidata", "wikidata")
+    repo = site.data_repository()
+
+    for idx, row in nouvelles_personnes.iterrows():
+        nomcomp = str(row["Prenom"]) + " " + str(row["Nom"]) if row["Prenom"] else str(row["Nom"])
+
+        new_item_id = create_item(repo, row, nomcomp)
+
+        datadf.loc[datadf["cmtqID"] == row["cmtqID"], "effectue"] = "X"
+        datadf.loc[datadf["cmtqID"] == row["cmtqID"], "qid"] = new_item_id
+
+        print(f"{nomcomp} : {new_item_id}")
+
+        datadf.to_csv("donnees/personnes_amapper-test.csv", index=False)
+
+
+if __name__ == "__main__":
+    donnees = "donnees/personnes_amapper-test.csv"
+    minoccs = 40
+    qlimit = 25
+
+    main(donnees, minoccs, qlimit)
